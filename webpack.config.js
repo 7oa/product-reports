a005829bccf202f6dcaf9bd89c2c42bb6388933b
anna <semenova.7oa@yandex.ru>
2020-04-07 10:56:39 +0300

    task

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [new OptimizeCssAssetsPlugin(), new TerserPlugin()];
+  }
+  return config;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: { bundle: ["@babel/polyfill", "./scripts/index.js"] },
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [autoprefixer()],
+              sourceMap: true,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
